dc27180f9db6e2acb2aadcbf8e3889cc556b28df
Mayur <mayurtyagi@unolo.com>
2024-09-04 19:17:40 +0530

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Mayur  on 04/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
