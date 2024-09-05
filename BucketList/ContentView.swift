af15dbfdd56b04142a8a147ab13ce1cc22909566
Mayur <mayurtyagi@unolo.com>
2024-09-05 19:31:04 +0530

    document directory code added

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,27 +7,28 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable{
-    var id = UUID()
-    var firstName: String
-    var lastName: String
-    
-    static func < (lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    var user = [
-        User(firstName: "Mayur", lastName: "A"),
-        User(firstName: "Manu", lastName: "C"),
-        User(firstName: "Mayur", lastName: "B")
-    ].sorted()
     
     var body: some View {
-        List(user){ user in
-            Text("\(user.firstName) \(user.lastName)")
+        let url = URL.documentsDirectory.appending(path: "mayur.text")
+        Button("Save Data"){
+            let data = Data("Hello Documents this file is edited".utf8)
+            do{
+                try data.write(to: url, options: [.atomic, .completeFileProtection])
+            }catch{
+                print(error.localizedDescription)
+            }
+            
         }
+        Button("Get Data"){
+            do{
+                let input = try String(contentsOf: url)
+                print(input)
+            }catch{
+                print(error.localizedDescription)
+            }
+        }
+        
     }
 }
 
